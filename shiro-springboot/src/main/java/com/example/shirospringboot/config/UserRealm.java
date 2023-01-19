cdbdb7d7ee963976f6e759b0448aa2e3649a04af
xiucai <Z123456789>
2023-01-19 22:09:37 +0800

    feat: 写死的密码授权认证成功

--- shiro-springboot/src/main/java/com/example/shirospringboot/config/UserRealm.java
+++ shiro-springboot/src/main/java/com/example/shirospringboot/config/UserRealm.java
@@ -3,6 +3,8 @@ package com.example.shirospringboot.config;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -26,8 +28,19 @@ public class UserRealm extends AuthorizingRealm {
      * 认证的方法
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         System.out.println("执行了认证的=>doGetAuthenticationInfo");
-        return null;
+
+        //用户名、密码
+        String username = "root";
+        String password = "123456";
+
+        UsernamePasswordToken userToken = (UsernamePasswordToken) token;
+        if (!userToken.getUsername().equals(username)) {
+            //这里shiro框架会自动帮助我们抛出 UnKnownAccountException
+            return null;
+        }
+
+        return new SimpleAuthenticationInfo("", password, "");
     }
 }
